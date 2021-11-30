859ef90d3b40e778bfd8d18dc9b1506905920e62
nforne <mnforne@gmail.com>
2021-11-30 05:46:06 -0800

    add serverdide check for user input

--- routes/orders.js
+++ routes/orders.js
@@ -3,37 +3,46 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.post("/", (req, res) => {
-      const order = req.body;
-      let orderf = {};
-      for (let i in order) {
-        if (typeof order[i] === 'object') {
-          orderf[i] = order[i]
-        }
+    const order = req.body;
+    
+    console.log('//--------------------------------');  
+    console.log(order);
+    console.log('//--------------------------------'); 
+    
+    
+    let orderf = {};
+    for (let i in order) {
+      if (typeof order[i] === 'object') {
+        orderf[i] = order[i]
       }
-      if (Object.keys(orderf).length === 0) {
-        const templateVars = { 
-          script: 'Oops! Could not understand your order. Please assign a quantity number to checked dishes only. We await you. Thank you!'
-        }
-        res.render('index', templateVars);
-      } else {
-        for (let i in orderf) {
-          for (let j of orderf[i]) {
-            if (typeof j === 'undefined') {
-              const templateVars = { 
-                script: 'Oops! Could not understand your order. Please assign a quantity number to checked dishes only. We await you. Thank you!'
-              }
-              res.render('index', templateVars);
-              break;
-            }
+    }
+    console.log('orderf -->', orderf);
+
+    if (Object.keys(orderf).length === 0) {
+      const templateVars = { 
+        script: 'Oops! Could not understand your order. Please assign a quantity to each checked dish only. We await you. Thank you!'
+      }
+      res.render('index', templateVars);
+    } else if (Object.keys(orderf).length !== 0) {
+      let faulty = false;
+      for (let i in orderf) {
+        for (let j of orderf[i]) {
+          if (j.length === 0) {
+            faulty = true;
+            break;
           }
         }
       }
-
-      console.log('//--------------------------------');  
-      console.log(order);
-      console.log('//--------------------------------'); 
-      res.render('index');  
-    });
+      if (faulty) {
+        const templateVars = { 
+          script: 'Oops! Could not understand your order. Please assign a quantity to each checked dish only. We await you. Thank you!'
+        }
+        res.render('index', templateVars);
+      } else {
+        res.render('index');
+      }
+    } 
+  });
   return router;
 };
   
